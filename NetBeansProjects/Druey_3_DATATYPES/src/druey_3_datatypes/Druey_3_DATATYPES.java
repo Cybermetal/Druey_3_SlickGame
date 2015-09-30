6abae7103007814015a75f9a291097c7fa246966
Cybermetal <adruey6760@stu.neisd.net>
2015-09-30 06:11:39 -0500

    Data Types

---
+++ NetBeansProjects/Druey_3_DATATYPES/src/druey_3_datatypes/Druey_3_DATATYPES.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package druey_3_datatypes;
+
+/**
+ *
+ * @author Cybermetal
+ */
+public class Druey_3_DATATYPES {
+
+    public static void main(String[] args) {
+        //this is a comment
+        //we'll use many comments to explain our code
+        //This program explores some of the primitave DATA Types
+        int i = 1;
+        float a = 2.5f;
+        double b = 3.87;
+        short c = 16; 
+        char d1 = ';';
+        char d2 = 'a';
+        char d3 = 42;
+        char d4 = 145;
+        String e = d4+"";
+        String e1 = "Adam is de almighty duckie";
+        boolean h = true;
+        boolean g = i % 2 == 0;
+        
+        /*
+        Here, we will look at some mathematical operators 
+        */
+        // + <---THIS adds two numbers N more
+        // - <---THIS subtracts two numbers N more
+        // * <---THIS multiplies two numbers N more
+        // / <---THIS divides two numbers or more
+        // % <-- This is modulo. It shows the remainder
+        
+        
+        
+        System.out.println(i + " an integer");
+        System.out.println(a + " a float");
+        System.out.println(b + " a double");
+        System.out.println(c + " a short");
+        System.out.println(d1 + " a char");
+        System.out.println(d2 + " a char");
+        System.out.println(d3 + " a char");
+        System.out.println(d4 + " a char");
+        System.out.println(e + " a String");
+        System.out.println(e1 + " a String");
+        System.out.println(h + " a boolean");
+        System.out.println(g + " a boolean");
+        
+ //      for(i = -123; i < 123; i++){
+        
+      
+    }
+    
+}
