bcae93143b4b0ebbe7e686aead4935c7477fed82
Cybermetal <adruey6760@stu.neisd.net>
2015-12-14 10:58:47 -0600

    Guessing Game

--- NetBeansProjects/Druey_3_GuessingGame/src/druey_3_guessinggame/Druey_3_GuessingGame.java
+++ NetBeansProjects/Druey_3_GuessingGame/src/druey_3_guessinggame/Druey_3_GuessingGame.java
@@ -5,15 +5,25 @@ import java.util.Scanner;
 
 public class Druey_3_GuessingGame { //Start of program
     
-    
+    static Scanner username = new Scanner(System.in);
+    static String name;
     static int magicnumber;
     static int userguess;
-    static String username;
+    
     static Random randy = new Random(10);
     static boolean playagain;
     
     public static void main(String[] args) { //Start of main class
-        playagain = true;
+        
+        
+    
+     name = username.nextLine();
+              System.out.println("Hello Would you like to play a game?");  
+                System.out.println("Whats your name?");
+                    System.out.println("Hello" +name+ "Lets begin");
+        
+   
+                playagain = true;
         magicnumber = randy.nextInt(10);
         Scanner number = new Scanner(System.in);
         System.out.println("Choose a number between 1 and 10");
@@ -21,24 +31,17 @@ public class Druey_3_GuessingGame { //Start of program
         int answer = 101;
         if(answer == i){
             System.out.println("why you do dis");
-        }else{
+        }
+        else{
             System.out.println("u ded heh, lol uninstall m8 ur bad");
-        Scanner userinputname = new Scanner(System.in);
-        Scanner sc = new Scanner (System.in);
-        sc.nextInt();
-        sc.nextLine();
-        username = userinputname.nextLine();
-        System.out.println("Hello " + username + " Let's play a game");
-        
-        /*while(playagain){
-            playthegame();
-            
-            
-        } */
+      //  Scanner userinputname = new Scanner(System.in);
+        //Scanner sc = new Scanner (System.in);
+        //sc.nextInt();
+        //sc.nextLine();    
         }
         
         
-        
-    } // End of main class
+        }
+}// End of main class
     
-} //End of program
+ //End of program
